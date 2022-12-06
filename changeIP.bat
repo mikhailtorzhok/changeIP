7d23da54554d0a77179da93307fc43482a00b461
Михаил Алекс. Морозов <Mikhail@torzhok-adm.local>
2022-12-06 10:55:58 +0300

    DNS settings

--- changeIP.bat
+++ changeIP.bat
@@ -18,11 +18,27 @@ if '%choice%'=='5' goto end
 ECHO "%choice%" is not valid, try again
 ECHO.
 goto start
+
 :con1
 ECHO Connecting Connection 1
+::set main IP, submusk and default gateway 
 netsh interface ip set address name ="Ethernet" static 192.168.0.137 255.255.255.0 192.168.0.5
+
+::set addition ip (Advansed button)
 netsh interface ipv4 add address name="Ethernet" address=192.168.8.137 255.255.255.0 192.168.8.5
+
+::clear DNS
+netsh interface ipv4 set dnsserver "Ethernet" static none
+:: set new first DNS
+netsh interface ipv4 add dns "Ethernet" 192.168.0.201 1
+:: set new alternative DNS
+netsh interface ipv4 add dns "Ethernet" 192.168.0.245 2
+
+
 goto end
+
+
+
 
 :con2
 ECHO Connecting Connection 2
